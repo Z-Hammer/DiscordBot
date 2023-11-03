45c2c5d65d96442e96d559007094307656950de5
Regulator <58798065+Z-Hammer@users.noreply.github.com>
2023-11-03 11:55:56 -0500

    Update Responses.py
    Deleted code

--- Responses.py
+++ Responses.py
@@ -1,10 +1,7 @@
 import CatAPI
 import AlcoholAPI
 import OuraAPI
-import Havenhaven
 
-# Austin username = goob3142#0
-# 'kitten' returns Cat Girl
 def get_response(user_message: str, message) -> str:
         p_message = user_message.lower()
         p_author = message.author
@@ -13,8 +10,5 @@ def get_response(user_message: str, message) -> str:
             return (CatAPI.get_cat_image())
         elif p_message == 'drink':
             return (AlcoholAPI.get_alcohol())
-        elif p_message[0:2] == 'hh':
-            search_phrase = p_message.split('hh')
-            return (Havenhaven.haven(search_phrase[1]))
         elif p_message == p_message:
             return (OuraAPI.oura_data(p_message))
