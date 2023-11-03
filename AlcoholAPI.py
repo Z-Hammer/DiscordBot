ea17f7b74dfe020dd907bc333c018143d5eec894
Regulator <58798065+Z-Hammer@users.noreply.github.com>
2023-11-03 12:00:41 -0500

    Update AlcoholAPI.py
    Added documentation

--- AlcoholAPI.py
+++ AlcoholAPI.py
@@ -11,24 +11,28 @@ def get_alcohol():
     measurements = []
     ingredients = []
     drink = data['drinks'][0]
+
+    # Find Ingredients
     for i in range(1, 10):
         if drink[f'strIngredient{i}'] == None:
             break
         else:
             ingredients.append(drink[f'strIngredient{i}'])
-    
+
+    # Find Measurements
     for i in range(1, 10):
         if drink[f'strMeasure{i}'] == None:
             break
         else:
             measurements.append(drink[f'strMeasure{i}'])
-    
+
+    # Format Ingredients with Measurements
     for i in range(len(ingredients)):
         try:
             together = together + (f"{ingredients[i]}   {measurements[i]}\n")
         except:
             together = together + (f"{ingredients[i]}   N/A\n")
-
+    
     if response.status_code == 200:
         return(f"{drink['strDrink']}\n\n{together}\n{drink['strInstructions']}")
     else:
